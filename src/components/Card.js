9de361437a2c327378d99975a16c740b0885f620
unknown <wf.nelu@yandex.ru>
2020-11-30 11:41:32 +0100

    review cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
 const Card = ({ details }) => {
+    const requireImage = (chemin) => {
+        try {
+            return require(`../img/${chemin}`)
+        } catch( error ) {
+            return require(`../img/default.jpeg`)
+        }
+    }
+
+    const ingredients = details.ingredients
+        .split(',')
+        .map(item =>
+            <li key={item.trim()}>{item.trim()}</li>
+        )
+
+    const instructions = details.instructions
+        .split('\n')
+        .map(item =>
+            <li key={item.trim()}>{item.trim()}</li>
+        )
+
     return (
         <div className="card">
+            <div className="image">
+                <img src={requireImage(details.image)} alt={details.nom} />
+            </div>
             <div className="recette">
                 <h2>{details.nom}</h2>
+                <ul className="liste-ingredients">
+                    {ingredients}
+                </ul>
+                <ol className="liste-instructions">
+                    {instructions}
+                </ol>
             </div>
         </div>
     );
